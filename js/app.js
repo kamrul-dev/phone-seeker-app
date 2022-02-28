9cbb2cbe2eb70dcebaa687392d2dc8bd4d6c3afa
Kamrul Hasan <kamrulsdev@gmail.com>
2022-03-01 00:08:40 +0600

    Mobile Basic info added in search result

--- js/app.js
+++ js/app.js
@@ -14,15 +14,14 @@ const displayPhone = (phones) => {
         const div = document.createElement('div');
         div.classList.add("col");
         div.innerHTML = `
-        <div class="card h-100 p-3">
+        <div class="phone-container h-100">
             <img src="${phone.image}" class="card-img-top" alt="...">
             <div class="card-body">
-                <h5 class="card-title">Card title</h5>
-                <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                <h5 class="card-title">${phone.phone_name}</h5>
+                <span class="badge bg-success">${phone.brand}</span>
             </div>
         </div>
         `;
         displaySearchResult.appendChild(div);
-
     })
 }
