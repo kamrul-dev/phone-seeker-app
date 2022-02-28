000ea676f235630997cf547da2959baa8ed5c90b
Kamrul Hasan <kamrulsdev@gmail.com>
2022-02-28 23:53:56 +0600

    display phone image on UI

--- js/app.js
+++ js/app.js
@@ -10,15 +10,19 @@ const searchPhone = () => {
 // display search result on UI
 const displayPhone = (phones) => {
     phones.forEach(phone => {
+        const displaySearchResult = document.getElementById('dispaly-phone');
         const div = document.createElement('div');
+        div.classList.add("col");
         div.innerHTML = `
-        <div class="card h-100">
-            <img src="" class="card-img-top" alt="...">
+        <div class="card h-100 p-3">
+            <img src="${phone.image}" class="card-img-top" alt="...">
             <div class="card-body">
                 <h5 class="card-title">Card title</h5>
                 <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
             </div>
         </div>
         `;
+        displaySearchResult.appendChild(div);
+
     })
 }
