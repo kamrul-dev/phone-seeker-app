a324b97823587cdc0be604ed6be7e8aef6a2e095
Kamrul Hasan <kamrulsdev@gmail.com>
2022-02-28 23:49:04 +0600

    UI setup done for display search result

--- js/app.js
+++ js/app.js
@@ -10,6 +10,15 @@ const searchPhone = () => {
 // display search result on UI
 const displayPhone = (phones) => {
     phones.forEach(phone => {
-        console.log(phone)
+        const div = document.createElement('div');
+        div.innerHTML = `
+        <div class="card h-100">
+            <img src="" class="card-img-top" alt="...">
+            <div class="card-body">
+                <h5 class="card-title">Card title</h5>
+                <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+            </div>
+        </div>
+        `;
     })
 }
